eec0d5ba06ff3a3bbd08ec65fce8ac9f536ba83c
murilott <murilo_teichert@hotmail.com>
2025-09-10 23:39:40 -0300

    feat: endpoint para debug

--- Pizzas Expert/authservice/src/main/java/com/example/authservice/interfaces/rest/HelloController.java
+++ Pizzas Expert/authservice/src/main/java/com/example/authservice/interfaces/rest/HelloController.java
@@ -1,13 +1,39 @@
 package com.example.authservice.interfaces.rest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+import com.example.authservice.application.user.ListUsersHandler;
+import com.example.authservice.interfaces.rest.dto.user.UserResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
 public class HelloController {
+    private final ListUsersHandler listUsersHandler;
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public ModelAndView list(Pageable pageable) {
+        Page<UserResponse> page = listUsersHandler.handle(pageable);
+
+        ModelAndView mv = new ModelAndView("users"); // users.html
+        mv.addObject("users", page.getContent());
+        mv.addObject("currentPage", page.getNumber());
+        mv.addObject("totalPages", page.getTotalPages());
+
+        return mv;
     }
+
+    // @GetMapping("/hello")
+    // public String hello() {
+    //     return "Hello World";
+    // }
 }
